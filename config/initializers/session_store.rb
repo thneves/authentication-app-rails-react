bc836ca1b44ad16a91132a96b788da13025ee070
thneves <tsneves11@gmail.com>
2020-12-01 12:56:40 -0300

    add condition to initializers to run app both in production or locally

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'authentication-app-react.herokuapp.com' #convention to start with underscore
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'authentication-app-react.herokuapp.com' #convention to start with underscore
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
